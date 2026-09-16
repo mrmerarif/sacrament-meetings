73b4a1ebc2ee5bc9cf484e4bcdcba31994beb855
mrmerarif <bflores2@byupathway.edu>
2026-09-16 12:42:22 -0600

    Fix CORS in meetings API route

--- app/api/meetings/route.ts
+++ app/api/meetings/route.ts
@@ -3,5 +3,12 @@ import { getMeetings } from "@/lib/meetings-db";
 export async function GET(request: Request) {
   const date = new URL(request.url).searchParams.get("date");
   const meetings = getMeetings(date);
-  return Response.json(meetings);
+
+  return new Response(JSON.stringify(meetings), {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+    },
+  });
 }
